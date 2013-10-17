8374eeeff07027bbb6b2ec3e12516c67b2e9d956
Sanae Rosen <sanae@umich.edu>
2013-10-17 17:02:44 -0400

    Documented RRCstates.py.  No functional changes.

--- server/gspeedometer/controllers/RRCstates.py
+++ server/gspeedometer/controllers/RRCstates.py
@@ -16,28 +16,51 @@ from google.appengine.api import taskqueue
 from gspeedometer import config
 
 class RRCStates(webapp.RequestHandler):
+   """ Interact with RRC-related data stored in the database"""
   
     def uploadRRCInference(self, **unused_args):
-        """Handler for uploadRRCInference request generated from client"""
+        """Handler for uploadRRCInference request generated from client.
+           Take the results of the RRC inference tasks and store them
+           in the database. 
+           Note that this is the result of *one* test, i.e. one sequence of
+           three packets sent.  A set of tests with varying inter-packet intervals
+           can be identified by all having the same test_id."""
+
         logging.info('Inside uploadRRCInference: input param: %s', self.request.body)        
         getRRCInferenceReqParam = json.loads(self.request.body)
         
         #read all the json param sent from the client.
         rawdata = model.RRCInferenceRawData()
+        # ID that uniquely identifies each phone, hashed
         rawdata.phone_id = util.HashDeviceId(str(getRRCInferenceReqParam['phone_id']))
+        # Test ID that is unique for each device and set of tests
         rawdata.test_id = getRRCInferenceReqParam['test_id']
         rawdata.network_type = getRRCInferenceReqParam['network_type']
+
+        # Round trip times for the small packets
         rawdata.rtt_low = getRRCInferenceReqParam['rtt_low']
+        # Round trip times for the large packets
         rawdata.rtt_high = getRRCInferenceReqParam['rtt_high']
+
+        # number of packets lost for the small packets        
         rawdata.lost_low = getRRCInferenceReqParam['lost_low']
+        # number of packets lost for the large packets
         rawdata.lost_high = getRRCInferenceReqParam['lost_high']
+
+        # signal strength at the time the small packets were sent
         rawdata.signal_low = getRRCInferenceReqParam['signal_low']
+        # signal strength at the time the large packets were sent
         rawdata.signal_high = getRRCInferenceReqParam['signal_high']
+
+        # Error values currently not implemented
         rawdata.error_low = getRRCInferenceReqParam['error_low']
         rawdata.error_high = getRRCInferenceReqParam['error_high']
+      
+        # The corresponding inter-packet interval
         rawdata.time_delay = getRRCInferenceReqParam['time_delay']
         #get the current time in utc
         rawdata.timestamp = datetime.datetime.utcnow()
+
         #write to DB/model
         rawdata.put()
          
@@ -55,7 +78,11 @@ class RRCStates(webapp.RequestHandler):
         return
         
     def getRRCmodel(self, **unused_args):
-        """Handler for getRRCmodel requests."""
+        """Handler for getRRCmodel requests.
+
+           Based on the data uploaded from the client, a model of RRC states and their
+           timers is built.  This fetches that model. This is only used internally by
+           the client to figure out when to run the upper-layer tests."""
         logging.info('Entering getRRCModel() handler:')
         if self.request.method.lower() != 'post':
             raise error.BadRequest('Not a POST request.')
@@ -86,6 +113,13 @@ class RRCStates(webapp.RequestHandler):
             
     
     def GetRRCmodelData(self,phone_id):
+        """ Helper function for getRRCmodel.  
+
+            Fetches the latest model created for a specific, given phone.
+            Returns a list of times, each of which are in the middle of an inferred
+            RRC state.  Used only by the client to determine the inter-packet intervals
+            for the upper-layer tests (TCP. HTTP, DNS).
+        """
         measurement_pts_list = []
         measurement_pts_dict = {}
 
@@ -94,12 +128,15 @@ class RRCStates(webapp.RequestHandler):
         logging.info('count of all rows ')
         logging.info(query.count())
         query.filter('phone_id =',phone_id)
+        # Since we don't care about average values corresponding to each timer, we
+        # arbitrarily select the rows that correspond to the values for small packets.
+        # Timers for large and small packets are the same.
         query.filter('small =',False)
         logging.info(query.count())       
         
         
         for row in query:
-            logging.info('Inside query iteration!! %d',row.segment_begin)
+            # Take a time in the middle of each segment
             segmentvalue = math.floor((row.segment_end - row.segment_begin)/2 + row.segment_begin)
             measurement_pts_list.append(segmentvalue)
         

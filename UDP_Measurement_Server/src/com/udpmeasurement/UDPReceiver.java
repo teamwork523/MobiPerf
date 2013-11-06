88481594a7f11b6f43f951eb91e3315ec2f8782a
laoyaosniper <laoyaosniper@gmail.com>
2013-11-06 11:37:26 -0500

    Hongyi: add lisence Apache 2.0 and email

--- UDP_Measurement_Server/src/com/udpmeasurement/UDPReceiver.java
+++ UDP_Measurement_Server/src/com/udpmeasurement/UDPReceiver.java
@@ -1,4 +1,17 @@
-// Copyright 2012 RobustNet Lab, University of Michigan. All Rights Reserved.
+/* Copyright 2013 RobustNet Lab, University of Michigan. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.udpmeasurement;
 
 import java.io.*;
@@ -9,7 +22,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author Hongyi Yao
+ * @author Hongyi Yao (hyyao@umich.edu)
  * The main receiver thread for UDP Burst Server. 
  * The thread continually receives the packet from client. If the packet
  * contains uplink data, it records the packet's information and send a
